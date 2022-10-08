9ced71750b1390b29429c4c768021a3c9c62e066
Semachew Fasika <eth.saf.1992@gmail.com>
2022-10-08 22:09:15 +0300

    user

--- models/user.py
+++ models/user.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""
+User Class from Models Module
+"""
+import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+from hashlib import md5
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    """User class handles all application users"""
+    if storage_type == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column("password", String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    def __init__(self, *args, **kwargs):
+        """
+        initialize User Model, inherits from BaseModel
+        """
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """
+        getter for password
+        :return: password (hashed)
+        """
+        return self.__dict__.get("password")
+
+    @password.setter
+    def password(self, password):
+        """
+        Password setter, with md5 hasing
+        :param password: password
+        :return: nothing
+        """
+        self.__dict__["password"] = md5(password.encode('utf-8')).hexdigest()
